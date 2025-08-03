cc5d6705f23d61ef563ab0414da5d0bc35b27545
Leomontenegro <leomontenegro6@gmail.com>
2025-08-03 16:43:06 -0300

    Portados diversos gráficos da versão européia para a americana.

--- Asm/graficos.asm
+++ Asm/graficos.asm
@@ -19,16 +19,46 @@
     .dw logo_tela_titulo
 .org 0x0825EAD8
     .dw nomes_tela_titulo
-;.org 0x0811BCCC
-;    .dw copyright
+.org 0x08160484
+    .dw copyright
+.org 0x080E5BC4
+    .dw nomes_tela_selecao_dados
+.org 0x082052DC
+    .dw menu_start_2
+.org 0x0827B204
+    .dw menu_start_3
+.org 0x0815E088
+    .dw menu_loja_hammer_1
+.org 0x0826032C
+    .dw localidade_castle_corridor
+.org 0x08260334
+    .dw localidade_chapel
+.org 0x0826033C
+    .dw localidade_study
+.org 0x08260344
+    .dw localidade_dance_hall
+.org 0x0826034C
+    .dw localidade_inner_quarters
+.org 0x08260354
+    .dw localidade_floating_garden
+.org 0x0826035C
+    .dw localidade_clock_tower
+.org 0x08260364
+    .dw localidade_underground_reservoir
+.org 0x0826036C
+    .dw localidade_underground_cemetery
+.org 0x08260374
+    .dw localidade_forbidden_area
+.org 0x0826037C
+    .dw localidade_the_arena
+.org 0x08260384
+    .dw localidade_top_floor
+.org 0x0826038C
+    .dw localidade_chaotic_realm
 ;.org 0x0815CDEC
 ;    .dw creditos
 ;.org 0x08164C08
 ;    .dw creditos_tilemap
-;.org 0x08014D14
-;    .dw tabela_vwf_fonte_grande
-;.org 0x08014f54
-;    .dw tabela_vwf_fonte_pequena
 
 ; Inserindo gráficos comprimidos editados no final da rom.
 .org 0x08FB5918
@@ -43,9 +73,78 @@ logo_tela_titulo:
 nomes_tela_titulo:
     .lz77gba "Graficos/Editados/0x63D848 - Nomes tela-titulo.gba"
     .align
-;copyright:
-;    .incbin "Graficos/Comprimidos/0xF9F598 - Copyright.gba"
-;    .align
+
+copyright:
+    .lz77gba "Graficos/Editados/0x5F6C60 - Copyright.gba"
+    .align
+
+nomes_tela_selecao_dados:
+    .lz77gba "Graficos/Editados/0x4F7B3C - Select Copy Delete Data.gba"
+    .align
+
+menu_start_2:
+    .lz77gba "Graficos/Editados/0x635990 - Menu Start 2.gba"
+    .align
+
+menu_start_3:
+    .lz77gba "Graficos/Editados/0x64F7E8 - Menu Start 3.gba"
+    .align
+
+menu_loja_hammer_1:
+    .lz77gba "Graficos/Editados/0x5C9BDC - Menu Loja Hammer 1.gba"
+    .align
+
+localidade_castle_corridor:
+    .lz77gba "Graficos/Editados/0x6409B0 - Localidade Castle Corridor.gba"
+    .align
+
+localidade_chapel:
+    .lz77gba "Graficos/Editados/0x640D5C - Localidade Chapel.gba"
+    .align
+
+localidade_study:
+    .lz77gba "Graficos/Editados/0x640FCC - Localidade Study.gba"
+    .align
+
+localidade_dance_hall:
+    .lz77gba "Graficos/Editados/0x641214 - Localidade Dance Hall.gba"
+    .align
+
+localidade_inner_quarters:
+    .lz77gba "Graficos/Editados/0x6414E0 - Localidade Inner Quarters.gba"
+    .align
+
+localidade_floating_garden:
+    .lz77gba "Graficos/Editados/0x641834 - Localidade Floating Garden.gba"
+    .align
+
+localidade_clock_tower:
+    .lz77gba "Graficos/Editados/0x641C18 - Localidade Clock Tower.gba"
+    .align
+
+localidade_underground_reservoir:
+    .lz77gba "Graficos/Editados/0x641F40 - Localidade Underground Reservoir.gba"
+    .align
+
+localidade_underground_cemetery:
+    .lz77gba "Graficos/Editados/0x6423D4 - Localidade Underground Cemetery.gba"
+    .align
+
+localidade_forbidden_area:
+    .lz77gba "Graficos/Editados/0x642850 - Localidade Forbidden Area.gba"
+    .align
+
+localidade_the_arena:
+    .lz77gba "Graficos/Editados/0x642BE0 - Localidade The Arena.gba"
+    .align
+
+localidade_top_floor:
+    .lz77gba "Graficos/Editados/0x642ECC - Localidade Top Floor.gba"
+    .align
+
+localidade_chaotic_realm:
+    .lz77gba "Graficos/Editados/0x6431AC - Localidade Chaotic Realm.gba"
+    .align
 
 ;creditos:
 ;    .incbin "Graficos/Comprimidos/0xFA85A4 - Creditos.gba"
